1011c8b286d50053f1f3bd27f7f68d60fe096553
Layne <pierce77@bu.edu>
2026-04-08 12:25:38 -0400

    Complete move_semantics exercises

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,4 +1,5 @@
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    // take a reference to the vector
     let mut vec = vec;
 
     vec.push(88);
@@ -20,7 +21,7 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
