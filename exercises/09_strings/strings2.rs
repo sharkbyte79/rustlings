9167101b2747650eadf904b5ca025b20a2dfe36a
Layne <pierce77@bu.edu>
2026-04-08 23:30:18 -0400

    Complete Strings exercises

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -6,7 +6,8 @@ fn is_a_color_word(attempt: &str) -> bool {
 fn main() {
     let word = String::from("green"); // Don't change this line.
 
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
+        // let the function borrow a reference
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
