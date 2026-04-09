1fae086a52ac3db5bd5a8ffa99878a893c69be7c
Layne <pierce77@bu.edu>
2026-04-09 10:31:14 -0400

    Complte hashmaps exercises

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        // match basket.get(&fruit).copied() {
+        //     Some(_) => continue,
+        //     None => basket.insert(fruit, 1),
+        // };
+        // Use entry API to insert this fruit if its not present
+        basket.entry(fruit).or_insert(1);
     }
 }
 
