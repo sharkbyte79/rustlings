557b9e1f68a241d7631ea9506696da623e5d3d34
Layne <pierce77@bu.edu>
2026-04-09 21:41:27 -0400

    Complete traits exercises

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -11,7 +11,8 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+// the type parameters can specify any type that implements the Licensed trait
+fn compare_license_types(software1: impl Licensed, software2: impl Licensed) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
